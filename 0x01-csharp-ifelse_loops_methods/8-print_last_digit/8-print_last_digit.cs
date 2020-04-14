53f4a4de424dc1467aa2a0b3735569317dabebc2
hacheG <943@holbertonschool.com>
2020-04-14 17:45:09 -0500

    task8

---
+++ 0x01-csharp-ifelse_loops_methods/8-print_last_digit/8-print_last_digit.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+class Number
+{
+    public static int PrintLastDigit(int number)
+    {
+        int val = number % 10;
+        if (val < 0)
+        {
+            val = val * -1;
+            Console.Write(val);
+            return val; 
+        }
+        Console.Write(val);
+        return val;
+    }
+}
