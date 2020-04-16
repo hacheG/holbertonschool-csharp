31335a4b93810af1e5e91b1b0b453c6c7039d9ce
hacheG <943@holbertonschool.com>
2020-04-16 18:25:37 -0500

    [FIX] add if index is out of range

--- 0x02-csharp-arrays_lists_dictionaries/2-replace_element/2-replace_element.cs
+++ 0x02-csharp-arrays_lists_dictionaries/2-replace_element/2-replace_element.cs
@@ -4,7 +4,15 @@ class Array
 {
     public static int[] ReplaceElement(int[] array, int index, int n)
     {
-        array[index] = n;
-        return array;
+        if (index < 0 || index > array.Length-1)
+        {
+            return array;
+        }
+        else
+        {
+            array[index] = n;
+            return array;
+        }
+        
     }
 }
