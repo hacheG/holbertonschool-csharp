b7beaa3560c4cdc191450087e6cb1cc8416c1b6f
hacheG <943@holbertonschool.com>
2020-04-16 21:59:44 -0500

    [FIX2] add if with return -1

--- 0x02-csharp-arrays_lists_dictionaries/5-max_integer/5-max_integer.cs
+++ 0x02-csharp-arrays_lists_dictionaries/5-max_integer/5-max_integer.cs
@@ -5,9 +5,16 @@ class List
 {
     public static int MaxInteger(List<int> myList)
     {
-        myList.Sort();
-        int max = myList[myList.Count-1];
-        return max;
-        
+        if (myList == 0 || myList == null)
+        {
+            Console.WriteLine("List is empty");
+            return -1;
+        }
+        else
+        {
+            myList.Sort();
+            int max = myList[myList.Count-1];
+            return max;
+        }
     }
 }
