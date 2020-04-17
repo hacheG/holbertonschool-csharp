39cb3c276621ff30f8135c49882feb4cd28bbb21
hacheG <943@holbertonschool.com>
2020-04-16 22:53:27 -0500

    [ADD] task6

---
+++ 0x02-csharp-arrays_lists_dictionaries/6-divisible_by_2/6-divisible_by_2.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+using System;
+
+class List
+{
+    public static List<bool> DivisibleBy2(List<int> myList)
+    {
+        List<bool> myListBool = new List<bool>();
+        for (int i = 0; i < myList.Count; i++)
+        {
+            if (myList[i] % 2 == 0){
+                myListBool.Add(true);
+            }
+            else{
+                myListBool.Add(false);
+            }
+        }
+        return myListBool;
+    }
+}
+
