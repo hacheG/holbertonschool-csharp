17c66e82d90e19b644be2e7ff67cc5dae200a2e4
hacheG <943@holbertonschool.com>
2020-04-16 20:51:43 -0500

    [ADD] task4

---
+++ 0x02-csharp-arrays_lists_dictionaries/4-print_list/4-print_list.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+
+class List
+{
+    public static List<int> CreatePrint(int size)
+    {
+        if (size < 0)
+        {
+            Console.WriteLine("Size cannot be negative");
+            return null;
+        }
+        else
+        {
+            List<int> miList = new List<int>();
+           for (int i = 0; i < size; i++)
+            {
+                miList.Add(i);
+                if ( i != size-1)
+                {
+                    Console.Write(miList[i] + " ");
+                }
+                else
+                {
+                    Console.Write(miList[i]);
+                }
+            }
+            Console.WriteLine("");
+            return miList;
+        }
+        
+        
+    }
+}
