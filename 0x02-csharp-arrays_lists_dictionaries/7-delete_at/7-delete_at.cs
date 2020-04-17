ca8b67dd0d86d123ac1f047d726448c807259d6c
hacheG <943@holbertonschool.com>
2020-04-16 23:15:37 -0500

    [ADD] task7

---
+++ 0x02-csharp-arrays_lists_dictionaries/7-delete_at/7-delete_at.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+using System;
+
+class List
+{
+    public static List<int> DeleteAt(List<int> myList, int index)
+    {
+        for (int i = 0; i < myList.Count; i++)
+        {
+            if (i == index)
+            {
+                myList.Remove(myList[i]);
+            }
+        }
+        return myList;
+    }
+}
+
