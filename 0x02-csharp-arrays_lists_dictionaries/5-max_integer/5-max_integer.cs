1a7d6c696e2e7f873f9aa16f5ccde8645f67399d
hacheG <943@holbertonschool.com>
2020-04-16 21:45:12 -0500

    [ADD] task5

---
+++ 0x02-csharp-arrays_lists_dictionaries/5-max_integer/5-max_integer.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+class PList
+{
+    public static int MaxInteger(List<int> myList)
+    {
+        int i, max = 0;
+
+        for (i = 0; i < myList.Count; i++)
+        {
+            if (myList[i] > max)
+            {
+                max = myList[i];
+            }
+        }
+        return max;
+    }
+}
