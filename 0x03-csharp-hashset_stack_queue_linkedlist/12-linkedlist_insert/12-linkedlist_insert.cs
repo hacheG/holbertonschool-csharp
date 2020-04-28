e0190041434c412fb43ed5462e1d8c6386ff0713
hacheG <943@holbertonschool.com>
2020-04-27 21:04:05 -0500

    [ADD] task

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/12-linkedlist_insert/12-linkedlist_insert.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+class LList
+{
+	public static LinkedListNode<int> Insert(LinkedList<int> myLList, int n)
+	{
+		LinkedListNode<int> current = myLList.First;
+		while (current != null)
+		{
+			if (n < current.Value)
+			{
+				return myLList.AddBefore(current, n);
+			}
+			current = current.Next;
+		}
+		return myLList.AddLast(n);
+	}
+}
