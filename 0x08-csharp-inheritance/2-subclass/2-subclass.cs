dcaa49c72ee5a0ab0a916a04e57ffe61ab2fff3c
hacheG <943@holbertonschool.com>
2020-05-07 20:16:24 -0500

    task

---
+++ 0x08-csharp-inheritance/2-subclass/2-subclass.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+///<summary>Class for object testing</summary>
+class Obj
+{
+	///<summary>check if its a subclass of base</summary>
+	///<returns>True or false</returns>
+	public static bool IsOnlyASubclass(Type derivedType, Type baseType)
+	{
+		return derivedType.IsSubclassOf(baseType);
+	}
+}
---
+++ 0x08-csharp-inheritance/8-shape/8-shape.cs
@@ -0,0 +1,91 @@
+﻿ 
+using System;
+
+///<summary>Class for Shapes</summary>
+class Shape
+{
+	//setting an error if method is not modified
+	public virtual int Area()
+	{
+		throw new System.NotImplementedException("Area() is not implemented");
+	}
+}
+
+///<summary>Class Rectangle inheriting from Shape</summary>
+class Rectangle : Shape
+{
+	private int width;
+	private int height;
+
+	//width of rectangle
+	public int Width
+	{
+		get
+		{
+			return width;
+		}
+		set
+		{
+			if (value < 0)
+			{
+				throw new ArgumentException("Width must be greater than or equal to 0");
+			}
+			width = value;
+		}
+	}
+
+	//height of rectangle
+	public int Height
+	{
+		get
+		{
+			return height;
+		}
+		set
+		{
+			if (value < 0)
+			{
+				throw new ArgumentException("Height must be greater than or equal to 0");
+			}
+			height = value;
+		}
+	}
+
+	//return Area of rectangle
+	public new int Area()
+	{
+		return width * height;
+	}
+
+	//overide string method
+	public override string ToString()
+	{
+		return String.Format("[Rectangle] {0} / {1}", width, height);
+	}
+}
+
+
+///<summary>Class Rectangle inheriting from Shape</summary>
+class Square : Rectangle
+{
+	private int size;
+
+	//size of width and height of square
+	public int Size
+	{
+		get
+		{
+			return size;
+		}
+		set
+		{
+			if (value < 0)
+			{
+				throw new ArgumentException("Size must be greater than or equal to 0");
+			}
+			size = value;
+			Height = value;
+			Width = value;
+		}
+	}
+}
