4368f0034e614c0d3237d921bb3fc1c52c0cc9ba
hacheG <943@holbertonschool.com>
2020-05-07 19:17:19 -0500

    [ADD] task

---
+++ 0x08-csharp-inheritance/4-inherit/4-inherit.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+
+///<summary>Base class Animal</summary>
+class Animal
+{
+
+}
+
+///<summary>Dog Class inheriting from Animal</summary>
+class Dog : Animal
+{
+
+}
---
+++ 0x08-csharp-inheritance/5-shape/5-shape.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+///<summary>Class for Shapes</summary>
+class Shape
+{
+	///<summary>Just throws error</summary>
+	public virtual int Area()
+	{
+		throw new System.NotImplementedException("Area() is not implemented");
+	}
+}
