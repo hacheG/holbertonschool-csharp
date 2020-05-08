79c369fab4938e72eef27c152dd202515c333924
hacheG <943@holbertonschool.com>
2020-05-07 19:54:34 -0500

    task

---
+++ 0x08-csharp-inheritance/0-is/0-is.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+///<summary>Class for object testing</summary>
+class Obj
+{
+	///<summary>check if its an integrer</summary>
+	///<returns>True or false</returns>
+	public static bool IsOfTypeInt(object obj)
+	{
+		return obj is int;
+	}
+}
